f9cae7c083b473e650347e24fbafdafc70457786
thodkatz <thodkatz@gmail.com>
2020-11-02 11:22:59 +0200

    Trying to talk to the port that ithaki opened to send the response

--- src/Client.java
+++ src/Client.java
@@ -14,46 +14,46 @@ class Client {
 				// INIT 
 				// TODO create a script that is scraping from ithaki website the request code and the ports.
 				// DatagramSocket sendSocket = new DatagramSocket();
-				DatagramSocket sendSocket = new DatagramSocket(48008); // bypassing port forwarding
-				String requestCode ="E6470";
+				//byte[] hostIP = { (byte)155, (byte)207,  (byte)18, (byte)208};
+				//InetAddress hostAddress = InetAddress.getByAddress(hostIP);
+				DatagramSocket sendSocket = new DatagramSocket(48013); // bypassing port forwarding. Binding the sendSocket to a specified socket
+				String requestCode ="E0984";
 				byte[] txbuffer = requestCode.getBytes();
-				int serverPort = 38008;
+				int serverPort = 38013;
 				byte[] hostIP = { (byte)155, (byte)207,  (byte)18, (byte)208};
 				InetAddress hostAddress = InetAddress.getByAddress(hostIP);
+				// alternative InetAddress hostAddress = InetAddress.getByName("ithaki.eng.auth.gr") (doesn't worked)
 				DatagramPacket sendPacket = new DatagramPacket(txbuffer, txbuffer.length, hostAddress, serverPort);
 
-				int clientPort = 48008;
-				DatagramSocket receiveSocket = new DatagramSocket(clientPort);
-				receiveSocket.setSoTimeout(3000);
+				//int clientPort = 48013;
+				//DatagramSocket receiveSocket = new DatagramSocket(clientPort);
+				//receiveSocket.setSoTimeout(3000);
+				sendSocket.setSoTimeout(3000);
 				byte[] rxbuffer = new byte[2048];
 				DatagramPacket receivePacket= new DatagramPacket(rxbuffer, rxbuffer.length);
 
-				System.out.println("Echo application...");
+				System.out.println("Echo application bypassing port forwarding...");
 				byte[] data = new byte[1024]; 
 				for(int i = 0; i<=4 ; i++) {
 					// ACTION
 					sendSocket.send(sendPacket);
+					System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my address is: " + sendSocket.getLocalAddress());
 					// LISTEN
-					receiveSocket.receive(receivePacket);
+					sendSocket.receive(receivePacket);
+					System.out.println("The port that opened ithaki to send the request is :" + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
 					data = receivePacket.getData();
 					String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
 					System.out.println("Ithaki responded with: " + message0);
 				}
 
-				// bypassing the port forwarding. But how? We know that ithaki send packets to a specific port. How ithaki can respond to the port automatically opens when sending packet.
-				System.out.println("Echo application but without port forwarding");
-				sendSocket.setSoTimeout(3000);
-				for(int i = 0; i<=4 ; i++) {
-					// ACTION
-					sendSocket.send(sendPacket);
-					data = sendPacket.getData();
-					String message1 = new String(data, StandardCharsets.US_ASCII);
-					System.out.println("The info of the send packet is: " + message1);
-					// LISTEN
-					sendSocket.receive(receivePacket);
-					data = receivePacket.getData();
-					String message2 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
-					System.out.println("Ithaki responded with: " + message2);
-				}
+				System.out.println("Trying to talk to ithaki to the port that is opened from the previous talk..");
+				serverPort = 58013;
+				sendPacket.setPort(serverPort);
+				sendSocket.send(sendPacket);
+				// where should I listen? sendSocket.set listening port
+				sendSocket.receive(receivePacket);
+				data = receivePacket.getData();
+				String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
+				System.out.println("Ithaki responded with: " + message0);
 		}
 }
