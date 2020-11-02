df63ca25f0141cbcced037bbb1bdfb9eb2c43c30
thodkatz <thodkatz@gmail.com>
2020-11-03 01:22:30 +0200

    Start working on image application
    Append byte after byte to create a byte string using the ByteArrayOutputStream

--- src/Client.java
+++ src/Client.java
@@ -13,21 +13,15 @@ class Client {
 
 				// INIT 
 				// TODO create a script that is scraping from ithaki website the request code and the ports.
-				// DatagramSocket sendSocket = new DatagramSocket();
 				byte[] clientIP = { (byte)192, (byte)168,  (byte)1, (byte)20};
 				InetAddress clientAddress = InetAddress.getByAddress(clientIP);
-				DatagramSocket sendSocket = new DatagramSocket(48032, clientAddress); 
-				String requestCode ="E7415";
+				DatagramSocket sendSocket = new DatagramSocket(48029, clientAddress); 
+				String requestCode ="E9105";
 				byte[] txbuffer = requestCode.getBytes();
-				int serverPort = 38032;
+				int serverPort = 38029;
 				byte[] hostIP = { (byte)155, (byte)207,  (byte)18, (byte)208};
 				InetAddress hostAddress = InetAddress.getByAddress(hostIP);
-				// alternative InetAddress hostAddress = InetAddress.getByName("ithaki.eng.auth.gr") (doesn't worked)
 				DatagramPacket sendPacket = new DatagramPacket(txbuffer, txbuffer.length, hostAddress, serverPort);
-				
-				//int clientPort = 48013;
-				//DatagramSocket receiveSocket = new DatagramSocket(clientPort);
-				//receiveSocket.setSoTimeout(3000);
 				sendSocket.setSoTimeout(4000);
 				byte[] rxbuffer = new byte[2048];
 				DatagramPacket receivePacket= new DatagramPacket(rxbuffer, rxbuffer.length);
@@ -37,14 +31,14 @@ class Client {
 				for(int i = 0; i<=4 ; i++) {
 					// ACTION
 					sendSocket.send(sendPacket);
-					System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my local address is: " + sendSocket.getLocalAddress());
+					//System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my local address is: " + sendSocket.getLocalAddress());
 					long timeBefore = System.currentTimeMillis();
-					System.out.println("My system time, when the request is sent,is: " + timeBefore);
+					System.out.println("My system time, when the request is sent, is: " + timeBefore);
 					// LISTEN
 					sendSocket.receive(receivePacket);
 					long timeAfter = System.currentTimeMillis();
 					System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
-					System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
+					//System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
 					data = receivePacket.getData();
 					String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
 					System.out.println("Ithaki responded with: " + message0);
@@ -52,5 +46,50 @@ class Client {
 
 
 				System.out.println("\nTemperature measurements...");
+				requestCode = "E9105T00";
+				txbuffer = requestCode.getBytes();
+				sendPacket.setData(txbuffer, 0, txbuffer.length);
+				for(int i = 0; i<=4 ; i++) {
+					// ACTION
+					sendSocket.send(sendPacket);
+					//System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my local address is: " + sendSocket.getLocalAddress());
+					long timeBefore = System.currentTimeMillis();
+					System.out.println("My system time, when the request is sent, is: " + timeBefore);
+					// LISTEN
+					sendSocket.receive(receivePacket);
+					long timeAfter = System.currentTimeMillis();
+					System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
+					//System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
+					data = receivePacket.getData();
+					String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
+					System.out.println("Ithaki responded with: " + message0);
+				}			
+
+
+				System.out.println("\nImage application...");
+				requestCode = "M9501";
+				txbuffer = requestCode.getBytes();
+				sendPacket.setData(txbuffer, 0, txbuffer.length);
+				sendSocket.send(sendPacket);	
+				long timeBefore = System.currentTimeMillis();
+				System.out.println("My system time, when the request is sent, is: " + timeBefore);
+			
+				ByteArrayOutputStream dataImage = new ByteArrayOutputStream();
+				for (;;) {
+						try {
+							sendSocket.receive(receivePacket);
+							long timeAfter = System.currentTimeMillis();
+							System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
+							dataImage.write(receivePacket.getData());
+							timeBefore = System.currentTimeMillis();
+						}
+						catch (Exception x) {
+								System.out.println(x);
+								break;
+						}
+				}	
+
+				byte[] dataImageBytes = dataImage.toByteArray();
+				System.out.println("How many bytes is the image? " + dataImageBytes.length);
 		}
 }
