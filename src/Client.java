580d64f7a917481181057fa7b137e166711628cd
thodkatz <thodkatz@gmail.com>
2020-11-03 14:31:59 +0200

    Bytes for the jpeg file are written successfully in a byte array
    The image isn't rendered properly because we didn't filter the received bytes and there are extra zeros from the initialization step

--- src/Client.java
+++ src/Client.java
@@ -15,10 +15,10 @@ class Client {
 				// TODO create a script that is scraping from ithaki website the request code and the ports.
 				byte[] clientIP = { (byte)192, (byte)168,  (byte)1, (byte)20};
 				InetAddress clientAddress = InetAddress.getByAddress(clientIP);
-				DatagramSocket sendSocket = new DatagramSocket(48029, clientAddress); 
-				String requestCode ="E9105";
+				DatagramSocket sendSocket = new DatagramSocket(48001, clientAddress); 
+				String requestCode ="E3092";
 				byte[] txbuffer = requestCode.getBytes();
-				int serverPort = 38029;
+				int serverPort = 38001;
 				byte[] hostIP = { (byte)155, (byte)207,  (byte)18, (byte)208};
 				InetAddress hostAddress = InetAddress.getByAddress(hostIP);
 				DatagramPacket sendPacket = new DatagramPacket(txbuffer, txbuffer.length, hostAddress, serverPort);
@@ -32,6 +32,11 @@ class Client {
 					// ACTION
 					sendSocket.send(sendPacket);
 					//System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my local address is: " + sendSocket.getLocalAddress());
+
+					requestCode = "E0000"; // disable server lag to respond
+					txbuffer = requestCode.getBytes();
+					sendPacket.setData(txbuffer, 0, txbuffer.length);
+
 					long timeBefore = System.currentTimeMillis();
 					System.out.println("My system time, when the request is sent, is: " + timeBefore);
 					// LISTEN
@@ -46,7 +51,7 @@ class Client {
 
 
 				System.out.println("\nTemperature measurements...");
-				requestCode = "E9105T00";
+				requestCode = "E3092T00";
 				txbuffer = requestCode.getBytes();
 				sendPacket.setData(txbuffer, 0, txbuffer.length);
 				for(int i = 0; i<=4 ; i++) {
@@ -67,7 +72,7 @@ class Client {
 
 
 				System.out.println("\nImage application...");
-				requestCode = "M9501";
+				requestCode = "M5068";
 				txbuffer = requestCode.getBytes();
 				sendPacket.setData(txbuffer, 0, txbuffer.length);
 				sendSocket.send(sendPacket);	
@@ -90,6 +95,27 @@ class Client {
 				}	
 
 				byte[] dataImageBytes = dataImage.toByteArray();
+				for (byte i : dataImageBytes) {
+					String hexa = String.format("%02X", i); // print hexadecimal the content of the byte array
+					System.out.print(hexa);
+				}
+
 				System.out.println("How many bytes is the image? " + dataImageBytes.length);
+				
+				File imageFile = new File("ithaki_image.jpg");
+				FileOutputStream fos = null;
+				try {
+						fos = new FileOutputStream(imageFile);
+						fos.write(dataImageBytes);
+						System.out.println("File has been written successfully");
+				}
+				catch (Exception x) {
+						System.out.println("Image application error:" + x);
+				}
+				finally {
+						if (fos != null) {
+								fos.close(); // close the OutputStream
+						}
+				}
 		}
 }
