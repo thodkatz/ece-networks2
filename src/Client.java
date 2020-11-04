82fd44152e525a0a76a5d6505392f97d226bce8f
thodkatz <thodkatz@gmail.com>
2020-11-04 21:39:52 +0200

    Getting started with Audio application

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.lang.System;
 import java.awt.Desktop;
+import javax.sound.sampled.*;
 
 class Client {
 		public static void main (String[] args) throws Exception {
@@ -36,10 +37,10 @@ class Client {
 				// TODO create a script that is scraping from ithaki website the request code and the ports.
 				byte[] clientIP = { (byte)192, (byte)168,  (byte)1, (byte)20};
 				InetAddress clientAddress = InetAddress.getByAddress(clientIP);
-				DatagramSocket sendSocket = new DatagramSocket(48019, clientAddress); 
-				String requestCode = "E4248";
+				DatagramSocket sendSocket = new DatagramSocket(48006, clientAddress); 
+				String requestCode = "E8101";
 				byte[] txbuffer = requestCode.getBytes();
-				int serverPort = 38019;
+				int serverPort = 38006;
 				byte[] hostIP = { (byte)155, (byte)207,  (byte)18, (byte)208};
 				InetAddress hostAddress = InetAddress.getByAddress(hostIP);
 				DatagramPacket sendPacket = new DatagramPacket(txbuffer, txbuffer.length, hostAddress, serverPort);
@@ -47,48 +48,60 @@ class Client {
 				byte[] rxbuffer = new byte[1024];
 				DatagramPacket receivePacket= new DatagramPacket(rxbuffer, rxbuffer.length);
 
-				System.out.println("Echo application...\n");
+				System.out.println("\n--------------------Echo application--------------------");
 				byte[] data = new byte[1024]; 
 				for(int i = 0; i<=4 ; i++) {
-					// ACTION
-					sendSocket.send(sendPacket);
-					System.out.println("The request code is: "+ requestCode + "\nThe destination port is: " + serverPort + "\nThe port that I opened to listen from ithaki is: " + sendSocket.getLocalPort() + "\nMy local address is: " + sendSocket.getLocalAddress());
+					try {
+						// ACTION
+						sendSocket.send(sendPacket);
+						System.out.println("The request code is: "+ requestCode + "\nThe destination port is: " + serverPort + "\nThe port that I opened to listen from ithaki is: " + sendSocket.getLocalPort() + "\nMy local address is: " + sendSocket.getLocalAddress());
 
-					requestCode = "E0000"; // disable server lag to respond
-					txbuffer = requestCode.getBytes();
-					sendPacket.setData(txbuffer, 0, txbuffer.length);
+						requestCode = "E0000"; // disable server lag to respond
+						txbuffer = requestCode.getBytes();
+						sendPacket.setData(txbuffer, 0, txbuffer.length);
 
-					long timeBefore = System.currentTimeMillis();
-					System.out.println("My system time, when the request is sent, is: " + timeBefore);
-					// LISTEN
-					sendSocket.receive(receivePacket);
-					long timeAfter = System.currentTimeMillis();
-					System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
-					//System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
-					data = receivePacket.getData();
-					String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
-					System.out.println("Ithaki responded with: " + message0);
+						long timeBefore = System.currentTimeMillis();
+						System.out.println("My system time, when the request is sent, is: " + timeBefore);
+						// LISTEN
+						sendSocket.receive(receivePacket);
+						long timeAfter = System.currentTimeMillis();
+						System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
+						//System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
+						data = receivePacket.getData();
+						String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
+						System.out.println("Ithaki responded with: " + message0);
+					}
+					catch (Exception x) {
+						System.out.println(x);
+						break;
+					}
 				}
 
 
-				System.out.println("\nTemperature measurements...");
-				requestCode = "E4248T00";
+				System.out.println("\n--------------------Temperature measurements--------------------");
+				requestCode = "E8101T00";
 				txbuffer = requestCode.getBytes();
 				sendPacket.setData(txbuffer, 0, txbuffer.length);
 				for(int i = 0; i<=4 ; i++) {
-					// ACTION
-					sendSocket.send(sendPacket);
-					//System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my local address is: " + sendSocket.getLocalAddress());
-					long timeBefore = System.currentTimeMillis();
-					System.out.println("My system time, when the request is sent, is: " + timeBefore);
-					// LISTEN
-					sendSocket.receive(receivePacket);
-					long timeAfter = System.currentTimeMillis();
-					System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
-					//System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
-					data = receivePacket.getData();
-					String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
-					System.out.println("Ithaki responded with: " + message0);
+					try {
+						// ACTION
+						sendSocket.send(sendPacket);
+						//System.out.println("The port that I opened to talk to ithaki is: " + sendSocket.getLocalPort() + " and my local address is: " + sendSocket.getLocalAddress());
+						long timeBefore = System.currentTimeMillis();
+						System.out.println("My system time, when the request is sent, is: " + timeBefore);
+						// LISTEN
+						sendSocket.receive(receivePacket);
+						long timeAfter = System.currentTimeMillis();
+						System.out.println("My system time, when the response received, is: " + timeAfter + " . So the time required to reveive a packet is: " + (timeAfter - timeBefore)/(float)1000 + " seconds"); 
+						//System.out.println("The port that opened ithaki to send the request is : " + receivePacket.getPort() + " and the address of ithaki is: " + receivePacket.getAddress());
+						data = receivePacket.getData();
+						String message0 = new String(data, StandardCharsets.US_ASCII); // convert binary to ASCI
+						System.out.println("Ithaki responded with: " + message0);
+					}
+					catch (Exception x) {
+						System.out.println(x);
+						break;
+					}
 				}			
 
 				//String[] imageRequestOptions = {"CAM=FIX", "CAM=PTZ", "CAM=PTZDIR=L", "CAM=PTZDIR=U", "CAM=PTZDIR=D", "CAM=PTZDIR=R"};
@@ -99,7 +112,7 @@ class Client {
 				for(int numImage = 0; numImage<Integer.parseInt(args[1]); numImage++) {
 
 					System.out.println("\n--------------------Image application---------------------");
-					requestCode = "M4197CAM=PTZUDP=1024DIR=" + args[0];
+					requestCode = "M1624CAM=PTZUDP=1024DIR=" + args[0];
 					//requestCode = "M4197CCAM=PTZUDP=1024DIR=R";
 					System.out.println("The request code is " + requestCode);
 					txbuffer = requestCode.getBytes();
@@ -112,59 +125,59 @@ class Client {
 							continue; // readjust the camera as many time is requested via the command line arguement. Print only the result, the last request
 					}
 
-					ByteArrayOutputStream dataImage = new ByteArrayOutputStream();
+					ByteArrayOutputStream bufferImage = new ByteArrayOutputStream();
 					int countPackets = 0;
 					long timeBefore = System.currentTimeMillis();
 					long timeBeforePerPacket = System.currentTimeMillis();
 					//System.out.println("My system time, when the request is sent, is: " + timeBefore);
 					outerloop:
 					for (;;) {
-							try {
-								sendSocket.receive(receivePacket);
+						try {
+							sendSocket.receive(receivePacket);
 					
-								long timeAfterPerPacket = System.currentTimeMillis();
-								System.out.println("The time required to reveive a packet is: " + (timeAfterPerPacket - timeBeforePerPacket)/(float)1000 + " seconds"); 
-								timeBeforePerPacket = System.currentTimeMillis();
+							long timeAfterPerPacket = System.currentTimeMillis();
+							System.out.println("The time required to reveive a packet is: " + (timeAfterPerPacket - timeBeforePerPacket)/(float)1000 + " seconds"); 
+							timeBeforePerPacket = System.currentTimeMillis();
 					
-								System.out.println("Packet No" + countPackets + ". The received bytes in hexadecimal format are:");
-								byte[] dataByte = receivePacket.getData();
-								for (int i = 0; i<receivePacket.getData().length; i++) {
-										String hexa = String.format("%02X", dataByte[i]); // convert bytes to hexa string 
-										System.out.print(hexa);
-										dataImage.write(dataByte[i]);
-										if ((String.format("%02X", dataByte[i]).equals("D9")) && (i!=0)) {
-											if ((String.format("%02X", dataByte[i-1]).equals("FF"))) {
-												break outerloop; // stop writing when EOF
-											}
+							System.out.println("Packet No" + countPackets + ". The received bytes in hexadecimal format are:");
+							byte[] dataImage = receivePacket.getData();
+							for (int i = 0; i<receivePacket.getData().length; i++) {
+									String hexa = String.format("%02X", dataImage[i]); // convert bytes to hexa string 
+									System.out.print(hexa);
+									bufferImage.write(dataImage[i]);
+									if ((String.format("%02X", dataImage[i]).equals("D9")) && (i!=0)) {
+										if ((String.format("%02X", dataImage[i-1]).equals("FF"))) {
+											break outerloop; // stop writing when EOF
 										}
-								}
-								//dataImage.write(receivePacket.getData()); // This is way more efficient though
-								System.out.println();
-								countPackets += 1;
+									}
 							}
-							catch (Exception x) {
-									System.out.println(x);
-									break;
-							}
+							//bufferImage.write(receivePacket.getData()); // This is way more efficient though
+							System.out.println();
+							countPackets++;
+						}
+						catch (Exception x) {
+								System.out.println(x);
+								break;
+						}
 					}	
 					long timeAfter = System.currentTimeMillis(); // get the time when the image is received in bytes
 					
 					System.out.println("Complete byte content of the image file in hexadecimal format:");
-					byte[] dataImageBytes = dataImage.toByteArray();
-					for (byte i : dataImageBytes) {
+					byte[] completeDataImage = bufferImage.toByteArray();
+					for (byte i : completeDataImage) {
 						String hexa = String.format("%02X", i); // print hexadecimal the content of the byte array
 						System.out.print(hexa);
 					}
 
 					System.out.println("\n\nTotal number of packages: " + (countPackets-1));
-					System.out.println("How many Kbytes is the image? " + dataImageBytes.length/(float)1000);
+					System.out.println("How many Kbytes is the image? " + completeDataImage.length/(float)1000);
 					
 					//File imageFile = new File("../media/image/sandbox/ithaki_image_PTZ_No" + numImage + ".jpg");
 					File imageFile = new File("../media/image/sandbox/ithaki_image_PTZ.jpg");
 					FileOutputStream fos = null;
 					try {
 							fos = new FileOutputStream(imageFile);
-							fos.write(dataImageBytes);
+							fos.write(completeDataImage);
 							System.out.println("File has been written successfully");
 					}
 					catch (Exception x) {
@@ -187,12 +200,61 @@ class Client {
 
 
 				System.out.println("\n--------------------Audio application--------------------");
-				requestCode = "A7163";
+				requestCode = "A8002";
 				txbuffer = requestCode.getBytes();
 				sendPacket.setData(txbuffer, 0, txbuffer.length);
+				sendSocket.send(sendPacket);	
+				
+				ByteArrayOutputStream bufferSound = new ByteArrayOutputStream(); // capture the audio in a buffer way
+
+				int countPackets = 0;
+				long timeBefore = System.currentTimeMillis();
+				long timeBeforePerPacket = System.currentTimeMillis();
+				for(;;) {
+						try {
+							sendSocket.receive(receivePacket);
+	
+							long timeAfterPerPacket = System.currentTimeMillis();
+							System.out.println("The time required to reveive a packet is: " + (timeAfterPerPacket - timeBeforePerPacket)/(float)1000 + " seconds"); 
+							timeBeforePerPacket = System.currentTimeMillis();
+					
+							System.out.println("Packet No" + countPackets + ". The received bytes in hexadecimal format are:");
+							byte[] dataSound  = receivePacket.getData();
+							bufferSound.write(dataSound);
+						}
+						catch (Exception x) {
+							System.out.println(x + "or ithaki finished sending audio packets");
+							break;
+						}
+						System.out.println();
+						countPackets++;
+				}
+
+				System.out.println("Complete byte content of the image file in hexadecimal format:");
+				byte[] completeDataSound = bufferSound.toByteArray();
+				for (byte i : completeDataSound) {
+					String hexa = String.format("%02X", i); // print hexadecimal the content of the byte array
+					System.out.print(hexa);
+				}
+
+				try {
+					AudioFormat modulationPCM = new AudioFormat(8000, 16, 1, true, false);
+					SourceDataLine outputAudio = AudioSystem.getSourceDataLine(modulationPCM);
+					//outputAudio.open(modulationPCM, 3200);
+					outputAudio.open(modulationPCM);
+					outputAudio.start();
+
+					outputAudio.write(completeDataSound, 0, completeDataSound.length);
+					outputAudio.stop();
+					outputAudio.close();
+					System.out.println("Sound application success!");
+
+				}
+				catch (Exception x) {
+					System.out.println(x + ". Sound playing failed");
+				}
 
 
-				//sendSocket.send(sendPacket);	
-				System.out.println("End of Java application");
+				System.out.println("\n--------------------END OF JAVA APPLICATION--------------------");
 		}
 }
