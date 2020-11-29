af3b5604c33f250584dcbb86728b551de6509835
thodkatz <thodkatz@gmail.com>
2020-11-29 17:44:17 +0200

    Add plots for session 2. Histos left

--- src/plots/plot.py
+++ src/plots/plot.py
@@ -23,13 +23,15 @@ import numpy as np
 #x = genfromtxt('../logs/session2/echo_throughput_delay.txt', delimiter=',')
 #x = genfromtxt('../logs/session2/echo_throughput_no_delay.txt', delimiter=',')
 #x = genfromtxt('../logs/AQFsamples.txt')
-#x = genfromtxt('../logs/Tsamples.txt')
-#x = genfromtxt('../logs/aqdpcm_step.txt')
+#x = genfromtxt('../logs/session2/Fsamples.txt')
+#x = genfromtxt('../logs/session2/Tsamples.txt')
+#x = genfromtxt('../logs/session2/second_clip/aqdpcm_mean.txt')
+#x = genfromtxt('../logs/session2/second_clip/aqdpcm_step.txt')
 #x = genfromtxt('../logs/second_clip/aqdpcm_step.txt')
 # plt.subplot(2,1,1)
 # plt.plot(x, 'y')
-# plt.xlabel('Time (sec)', fontsize=12)
-# plt.ylabel('Throughput', fontsize=12)
+# plt.xlabel('Number of samples', fontsize=12)
+# plt.ylabel('Step Values', fontsize=12)
 # plt.grid(True)
 
 # plt.subplot(2,1,2)
@@ -42,22 +44,22 @@ import numpy as np
 
 
 #Retransmission timeout plot
-data = genfromtxt('../logs/session2/rto.txt', delimiter=' ')
-rtt = data[1:,0]
-srtt = data[1:,1]
-rttd = data[1:,2]
-rto = data[1:,3]
-plt.plot(rtt, label = "RTT")
-plt.plot(srtt, label = "SRTT")
-plt.plot(rttd, label = "RTTd")
-plt.plot(rto, label = "RTO")
-plt.xlabel('Number of packets', fontsize=12)
-plt.ylabel("Time response", fontsize=12)
-plt.legend()
-plt.show()
+# data = genfromtxt('../logs/session2/rto.txt', delimiter=' ')
+# rtt = data[1:,0]
+# srtt = data[1:,1]
+# rttd = data[1:,2]
+# rto = data[1:,3]
+# plt.plot(rtt, label = "RTT")
+# plt.plot(srtt, label = "SRTT")
+# plt.plot(rttd, label = "RTTd")
+# plt.plot(rto, label = "RTO")
+# plt.xlabel('Number of packets', fontsize=12)
+# plt.ylabel("Time response", fontsize=12)
+# plt.legend()
+# plt.show()
 
 # Copter
-# data = genfromtxt('../logs/copter_info.txt', delimiter=' ')
+# data = genfromtxt('../logs/session2/copter_2nd_run/copter_info.txt', delimiter=' ')
 # rtt = data[1:,0]
 # srtt = data[1:,1]
 # rttd = data[1:,2]
@@ -70,32 +72,32 @@ plt.show()
 # plt.ylabel("Data", fontsize=12, labelpad=10)
 # plt.legend()
 # plt.grid(True);
-# plt.yticks(np.arange(0, 1200, 100))
+# plt.yticks(np.arange(0, 1200, 50))
 # plt.show()
 
 #Vehicle
-#data = genfromtxt('../logs/session1/car_telemetry.txt', delimiter=' ')
-#rtt = data[0:,0]
-#srtt = data[0:,1]
-#rttd = data[0:,2]
-#rto = data[0:,3]
-#s = data[0:, 4]
-#t = data[0:, 5]
-#plt.subplot(211)
-#plt.plot(rtt, label = "Engine run time")
-#plt.plot(rto, label = "Engine RPM")
-#plt.xlabel('Number of packets', fontsize=12, labelpad=10)
-#plt.ylabel("Data", fontsize=12, labelpad=10)
-#plt.legend()
-#plt.grid(True);
-#plt.subplot(212)
-#plt.plot(srtt, label = "Intake air temperature")
-#plt.plot(rttd, label = "Throttle position")
-#plt.plot(s, label = "Vehicle speed")
-#plt.plot(t, label = "Coolant temperature")
-#plt.xlabel('Number of packets', fontsize=12, labelpad=10)
-#plt.ylabel("Data", fontsize=12, labelpad=10)
-#plt.legend()
-#plt.grid(True);
-##plt.yticks(np.arange(0, 2500, 25))
-#plt.show()
+data = genfromtxt('../logs/session2/car_telemetry.txt', delimiter=' ')
+rtt = data[0:,0]
+srtt = data[0:,1]
+rttd = data[0:,2]
+rto = data[0:,3]
+s = data[0:, 4]
+t = data[0:, 5]
+plt.subplot(211)
+plt.plot(rtt, label = "Engine run time")
+plt.plot(rto, label = "Engine RPM")
+plt.xlabel('Number of packets', fontsize=12, labelpad=10)
+plt.ylabel("Data", fontsize=12, labelpad=10)
+plt.legend()
+plt.grid(True);
+plt.subplot(212)
+plt.plot(srtt, label = "Intake air temperature")
+plt.plot(rttd, label = "Throttle position")
+plt.plot(s, label = "Vehicle speed")
+plt.plot(t, label = "Coolant temperature")
+plt.xlabel('Number of packets', fontsize=12, labelpad=10)
+plt.ylabel("Data", fontsize=12, labelpad=10)
+plt.legend()
+plt.grid(True);
+#plt.yticks(np.arange(0, 2500, 25))
+plt.show()
